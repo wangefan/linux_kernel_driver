1ff32f07a1850d9cb38fbbf7ba7a0bb7a3a746d1
Yifan Wang <yifan.wang@xperi.com>
2024-12-10 15:17:57 +0800

    Initialize and add cdev

--- dyn_chr_dev/dyn_chr_dev.c
+++ dyn_chr_dev/dyn_chr_dev.c
@@ -7,8 +7,15 @@ dev_t dyn_chr_dev_id = 0;
 static int major = 0; // Major number, 0 means dynamic allocation
 static int minor = 0; // Minor number, 0 means dynamic allocation
 static char *dyn_dev_name = "dyn_chr_dev";
-static struct class *dyn_dev_class;
+
+/* For char dev */
 static struct cdev dyn_chr_cdev;
+static struct file_operations dyn_chr_fops =
+{
+  .owner = THIS_MODULE,
+};
+
+static struct class *dyn_dev_class;
 
 static struct task_struct *worker_thread;
 
@@ -31,13 +38,30 @@ static int __init dyn_chr_dev_init(void) {
     minor = MINOR(dyn_chr_dev_id);
     printk(KERN_INFO "alloc_chrdev_region: major=%d minor=%d\n", major, minor);
   }
+
+  // Initialize cdev
+  cdev_init(&dyn_chr_cdev, &dyn_chr_fops);
+  dyn_chr_cdev.owner = THIS_MODULE;
+  if(cdev_add(&dyn_chr_cdev, dyn_chr_dev_id, 1) < 0) {
+    printk(KERN_ERR "cdev_add failed\n");
+    goto unregister_chrdev;
+  }
+  printk(KERN_INFO "Initialize and add cdev ok\n");
   return 0;
+
+cleanup_cdev:
+  cdev_del(&dyn_chr_cdev);
+unregister_chrdev:
+  unregister_chrdev_region(dyn_chr_dev_id, 1);
+  printk(KERN_ERR "dyn_chr_dev_init failed\n");
+  return -1;
 }
 
 /* Exit */
 static void __exit dyn_chr_dev_exit(void) {
-  printk(KERN_INFO "dyn_chr_dev_exit\n");
+  cdev_del(&dyn_chr_cdev);
   unregister_chrdev_region(dyn_chr_dev_id, 1);
+  printk(KERN_INFO "dyn_chr_dev_exit ok\n");
 }
 
 module_init(dyn_chr_dev_init);
