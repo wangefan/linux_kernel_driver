4ea8ee32346f98677092839f4f5b8e2917ee3c66
Yifan Wang <yifan.wang@xperi.com>
2024-12-10 15:47:44 +0800

    Create class and device

--- dyn_chr_dev/dyn_chr_dev.c
+++ dyn_chr_dev/dyn_chr_dev.c
@@ -1,20 +1,22 @@
+#include <linux/cdev.h>
+#include <linux/fs.h>
 #include <linux/init.h>
 #include <linux/module.h>
-#include <linux/fs.h>
-#include <linux/cdev.h>
 
 dev_t dyn_chr_dev_id = 0;
 static int major = 0; // Major number, 0 means dynamic allocation
 static int minor = 0; // Minor number, 0 means dynamic allocation
 static char *dyn_dev_name = "dyn_chr_dev";
+static char *dyn_dev_class_name = "dyn_chr_dev_class";
+static char *dyn_dev_device_name = "dyn_chr_dev_device";
 
 /* For char dev */
 static struct cdev dyn_chr_cdev;
-static struct file_operations dyn_chr_fops =
-{
-  .owner = THIS_MODULE,
+static struct file_operations dyn_chr_fops = {
+    .owner = THIS_MODULE,
 };
 
+/* For auto creating device without mknod */
 static struct class *dyn_dev_class;
 
 static struct task_struct *worker_thread;
@@ -42,13 +44,32 @@ static int __init dyn_chr_dev_init(void) {
   // Initialize cdev
   cdev_init(&dyn_chr_cdev, &dyn_chr_fops);
   dyn_chr_cdev.owner = THIS_MODULE;
-  if(cdev_add(&dyn_chr_cdev, dyn_chr_dev_id, 1) < 0) {
+  if (cdev_add(&dyn_chr_cdev, dyn_chr_dev_id, 1) < 0) {
     printk(KERN_ERR "cdev_add failed\n");
     goto unregister_chrdev;
   }
   printk(KERN_INFO "Initialize and add cdev ok\n");
+
+  // Creating struct class
+  if (IS_ERR(dyn_dev_class = class_create(THIS_MODULE, dyn_dev_class_name))) {
+    printk(KERN_ERR "Cannot create the struct class\n");
+    goto cleanup_cdev;
+  }
+
+  // Creating device
+  if (IS_ERR(device_create(dyn_dev_class, NULL, dyn_chr_dev_id, NULL,
+                           dyn_dev_device_name))) {
+    printk(KERN_ERR "Cannot create the device\n");
+    goto cleanup_class;
+  }
+
+  printk(KERN_INFO "Create class and device ok\n");
   return 0;
 
+cleanup_device:
+  device_destroy(dyn_dev_class, dyn_chr_dev_id);
+cleanup_class:
+  class_destroy(dyn_dev_class);
 cleanup_cdev:
   cdev_del(&dyn_chr_cdev);
 unregister_chrdev:
@@ -59,6 +80,8 @@ unregister_chrdev:
 
 /* Exit */
 static void __exit dyn_chr_dev_exit(void) {
+  device_destroy(dyn_dev_class, dyn_chr_dev_id);
+  class_destroy(dyn_dev_class);
   cdev_del(&dyn_chr_cdev);
   unregister_chrdev_region(dyn_chr_dev_id, 1);
   printk(KERN_INFO "dyn_chr_dev_exit ok\n");
--- dyn_chr_dev/scpko.sh
+++ dyn_chr_dev/scpko.sh
@@ -6,5 +6,13 @@ sudo depmod
 sudo modprobe dyn_chr_dev
 sudo rmmod dyn_chr_dev
 
+$ ls -al /sys/class
+..
+drwxr-xr-x  2 root root 0 十二  9 08:31 drm_dp_aux_dev
+drwxr-xr-x  2 root root 0 十二 10 15:36 dyn_chr_dev_class
+
+$ ls -al /dev/ | grep dyn_
+crw-------   1 root root    509,     0 十二 10 15:36 dyn_chr_dev_device
+
 cat /proc/sys/kernel/printk
 echo 7 > /proc/sys/kernel/printk
