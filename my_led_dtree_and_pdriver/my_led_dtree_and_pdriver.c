595a568593ba447b9efcde9ea52f3ae20f58c10c
Yifan Wang <yifan.wang@xperi.com>
2025-04-07 11:51:44 +0800

    introduce /sys/class/my_led/led_ctrl
    check:
    insmod <*.ko> => /sys/class/my_led/led_ctrl created
    rmmod <*.ko> => /sys/class/my_led/led_ctrl removed

--- my_led_dtree_and_pdriver/my_led_dtree_and_pdriver.c
+++ my_led_dtree_and_pdriver/my_led_dtree_and_pdriver.c
@@ -7,6 +7,23 @@
 #define led_err(fmt, ...) pr_err("[my_LED]: " fmt, ##__VA_ARGS__)
 
 #define DRIVER_NAME "my_led_dtree_and_pdriver"
+#define CLASS_NAME "my_led_class"
+
+// define file mode, all readable, user and group are writiable
+#define MY_LED_RW_ATTR (S_IRUSR | S_IRGRP | S_IROTH | S_IWUSR | S_IWGRP)
+
+static ssize_t my_led_show(struct class *class, struct class_attribute *attr,
+                           char *buf);
+static ssize_t my_led_store(struct class *class, struct class_attribute *attr,
+                            const char *buf, size_t count);
+
+static struct class_attribute my_led_class_attrs[] = {
+    __ATTR(led_ctrl, MY_LED_RW_ATTR, my_led_show, my_led_store),
+};
+
+static struct class my_led_class = {
+    .name = CLASS_NAME,
+};
 
 static const struct of_device_id my_led_dt_match[] = {
     {
@@ -15,13 +32,66 @@ static const struct of_device_id my_led_dt_match[] = {
     {},
 };
 
+static ssize_t my_led_show(struct class *class, struct class_attribute *attr,
+                           char *buf) {
+  led_info("show...\n");
+  return sprintf(buf, "LED status: ON\n");
+}
+
+static ssize_t my_led_store(struct class *class, struct class_attribute *attr,
+                            const char *buf, size_t count) {
+  led_info("store...\n");
+  if (strncmp(buf, "on", 2) == 0) {
+    led_info("LED ON\n");
+  } else if (strncmp(buf, "off", 3) == 0) {
+    led_info("LED OFF\n");
+  } else {
+    led_err("Invalid command\n");
+    return -EINVAL;
+  }
+  return count;
+}
+
 static int my_led_probe(struct platform_device *pdev) {
+  int ret = -1;
+  int class_attr_num = 0, idx_class_attr = 0;
+
   led_info("probe...\n");
-  return 0;
+
+  // register the class
+  ret = class_register(&my_led_class);
+  if (ret) {
+    led_err("class_register failed!\n");
+    return ret;
+  }
+
+  // create the class
+  class_attr_num = ARRAY_SIZE(my_led_class_attrs);
+  for (idx_class_attr = 0; idx_class_attr < class_attr_num; idx_class_attr++) {
+    ret = class_create_file(&my_led_class, &my_led_class_attrs[idx_class_attr]);
+    if (ret) {
+      led_err("class_create_file failed!\n");
+      goto err;
+    }
+  }
+
+  led_info("probe ok\n");
+  return ret;
+err:
+  while (--idx_class_attr >= 0)
+    class_remove_file(&my_led_class, &my_led_class_attrs[idx_class_attr]);
+  class_unregister(&my_led_class);
+  return ret;
 }
 
 static int my_led_remove(struct platform_device *pdev) {
+  int idx_class_attr = 0;
   led_info("remove...\n");
+  for (idx_class_attr = 0; idx_class_attr < ARRAY_SIZE(my_led_class_attrs);
+       idx_class_attr++)
+    class_remove_file(&my_led_class, &my_led_class_attrs[idx_class_attr]);
+  class_unregister(&my_led_class);
+  led_info("remove ok\n");
   return 0;
 }
 
